018793ffda35438702469214cdd86ae07c5ec941
ashup <ashupal0089@gmail.com>
2024-02-04 20:51:48 +0530

    4-02-24

--- lib/Lecture 5.4/containerList.dart
+++ lib/Lecture 5.4/containerList.dart
@@ -51,3 +51,222 @@ List containers = [
     ),
   },
 ];
+
+List iconlist = [
+  {
+    'icon1': Icon(
+      Icons.add,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon2': Icon(
+      Icons.adjust,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon3': Icon(
+      Icons.arrow_back_ios,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon4': Icon(
+      Icons.arrow_forward_ios,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon5': Icon(
+      Icons.alarm,
+      size: 35,
+      color: Colors.grey.shade700,
+    )
+  },
+  {
+    'icon1': Icon(
+      Icons.privacy_tip,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon2': Icon(
+      Icons.account_circle,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon3': Icon(
+      Icons.sync,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon4': Icon(
+      Icons.shuffle,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon5': Icon(
+      Icons.swipe_up,
+      size: 35,
+      color: Colors.grey.shade700,
+    )
+  },
+  {
+    'icon1': Icon(
+      Icons.bookmark,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon2': Icon(
+      Icons.more_vert,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon3': Icon(
+      Icons.zoom_in,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon4': Icon(
+      Icons.arrow_circle_left,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon5': Icon(
+      Icons.back_hand,
+      size: 35,
+      color: Colors.grey.shade700,
+    )
+  },
+  {
+    'icon1': Icon(
+      Icons.call,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon2': Icon(
+      Icons.search,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon3': Icon(
+      Icons.tab,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon4': Icon(
+      Icons.smart_button,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon5': Icon(
+      Icons.back_hand,
+      size: 35,
+      color: Colors.grey.shade700,
+    )
+  },
+  {
+    'icon1': Icon(
+      Icons.wifi,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon2': Icon(
+      Icons.ac_unit_sharp,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon3': Icon(
+      Icons.cable_rounded,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon4': Icon(
+      Icons.delete,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon5': Icon(
+      Icons.earbuds,
+      size: 35,
+      color: Colors.grey.shade700,
+    )
+  },
+  {
+    'icon1': Icon(
+      Icons.h_plus_mobiledata_outlined,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon2': Icon(
+      Icons.kayaking,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon3': Icon(
+      Icons.e_mobiledata,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon4': Icon(
+      Icons.safety_divider,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon5': Icon(
+      Icons.inbox,
+      size: 35,
+      color: Colors.grey.shade700,
+    )
+  },
+  {
+    'icon1': Icon(
+      Icons.vaccines,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon2': Icon(
+      Icons.dashboard,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon3': Icon(
+      Icons.gamepad,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon4': Icon(
+      Icons.join_inner,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon5': Icon(
+      Icons.zoom_out,
+      size: 35,
+      color: Colors.grey.shade700,
+    )
+  },
+  {
+    'icon1': Icon(
+      Icons.image,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon2': Icon(
+      Icons.qr_code_scanner_sharp,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon3': Icon(
+      Icons.radar,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon4': Icon(
+      Icons.u_turn_left,
+      size: 35,
+      color: Colors.grey.shade700,
+    ),
+    'icon5': Icon(
+      Icons.offline_bolt,
+      size: 35,
+      color: Colors.grey.shade700,
+    )
+  }
+];
